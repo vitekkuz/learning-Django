3ea9aba772c6f7a23749e13f49651b1acbad7eca
vitekkuz <vitekkuz@mail.ru>
2022-06-18 23:16:02 +0300

    very simple. 1 app render 1 html-string

---
+++ simpleoutput/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SimpleoutputConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'simpleoutput'
---
+++ simpleoutput/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+
+def index(request):
+    return HttpResponse('<h1> Hello! </h1>')
+
